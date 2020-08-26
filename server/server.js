aa7cd475dfdbf3d3ca8500e157ed414b3bf268bf
zachsnoek <dev@zachsnoek.com>
2020-08-25 21:20:50 -0700

    Add rate limiter to contact route

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const rateLimit = require("express-rate-limit");
 const connectDB = require("./config/db");
 const errorHandler = require("./middleware/errorHandler");
 
@@ -19,12 +20,19 @@ connectDB();
 // Express Middleware
 app.use(express.json());
 
+// Rate limiter
+const apiLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+});
+
 // Mount routers
 const API_PRE = "/api/v1";
 app.use(`${API_PRE}/auth`, require("./routes/auth"));
 app.use(`${API_PRE}/portfolio`, require("./routes/portfolio"));
 app.use(`${API_PRE}/blog`, require("./routes/blog"));
 app.use(`${API_PRE}/contact`, require("./routes/contact"));
+app.use(`${API_PRE}/contact`, apiLimiter);
 
 app.use(errorHandler);
 
