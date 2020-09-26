5657ea1cc7f7b643d1a1849ad89e9f7866c86569
zachsnoek <dev@zachsnoek.com>
2020-09-26 15:45:47 -0700

    Add loading spinner

---
+++ client/src/components/shared/LoadingSpinner/LoadingSpinner.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import CenterContainer from "../CenterContainer/CenterContainer";
+import "./styles.css";
+
+const LoadingSpinner = () => {
+    return <CenterContainer className="hexdots-loader"></CenterContainer>;
+};
+
+export default LoadingSpinner;
