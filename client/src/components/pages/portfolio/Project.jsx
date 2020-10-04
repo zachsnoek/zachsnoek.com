b8fb0827e2e50774dfdc3f56b601e30eacafc31e
jonathanchaffer <jonathanchaffer@gmail.com>
2020-10-04 16:41:50 -0400

    Add masonry layout to Portfolio.jsx

--- client/src/components/pages/portfolio/Project.jsx
+++ client/src/components/pages/portfolio/Project.jsx
@@ -6,24 +6,22 @@ import { BASE_URL } from "../../../utils/api";
 
 const Project = ({ title, description, image, github, website, tags }) => {
     return (
-        <div className="grid-item col-lg-6 col-xl-4">
-            <div className="card">
-                <div className="card-header">
-                    <span className="card-title">{title}</span>
-                </div>
-                <img
-                    src={`${BASE_URL}/${image}`}
-                    alt="A screenshot of a portfolio project."
-                />
-                <div className="card-body">{description}</div>
-                <div className="card-footer">
-                    <div className="row">
-                        <div className="col">
-                            <Tags tags={tags} />
-                        </div>
-                        <div className="col-auto">
-                            <Links github={github} website={website} />
-                        </div>
+        <div className="card">
+            <div className="card-header">
+                <span className="card-title">{title}</span>
+            </div>
+            <img
+                src={`${BASE_URL}/${image}`}
+                alt="A screenshot of a portfolio project."
+            />
+            <div className="card-body">{description}</div>
+            <div className="card-footer">
+                <div className="row">
+                    <div className="col">
+                        <Tags tags={tags} />
+                    </div>
+                    <div className="col-auto">
+                        <Links github={github} website={website} />
                     </div>
                 </div>
             </div>
