8e3b1a9db3ed34d38e0f82ddfd9e30807fcc7d23
zachsnoek <dev@zachsnoek.com>
2020-10-21 20:16:27 -0700

    Add document title for index path

--- client/src/utils/title.js
+++ client/src/utils/title.js
@@ -10,22 +10,21 @@ const capitalize = (string) => string.charAt(0).toUpperCase() + string.slice(1);
 export const pathToTitle = (pathname) => {
     const basePath = pathname.split("/", 2)[1];
 
-    if (basePath !== "") {
-        const parts = basePath.split("-");
-
-        return parts.length === 1
-            ? capitalize(basePath)
-            : parts.reduce(
-                  (acc, part) => capitalize(acc) + " " + capitalize(part)
-              );
+    // Index title
+    if (basePath === "") {
+        return "Developer";
     }
+
+    const parts = basePath.split("-");
+
+    return parts.length === 1
+        ? capitalize(basePath)
+        : parts.reduce((acc, part) => capitalize(acc) + " " + capitalize(part));
 };
 
 export const setTitle = ({ pathname }) => {
     if (validatePath(pathname)) {
         const title = pathToTitle(pathname);
-        document.title = `${titlePrefix} ${title ? "| " + title : ""}`;
-    } else {
-        document.title = titlePrefix;
+        document.title = `${titlePrefix} | ${title}`;
     }
 };
