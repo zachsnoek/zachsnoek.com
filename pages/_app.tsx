fb56a0d47f57514d4b275ea57da54566ab88b516
zachsnoek <dev@zachsnoek.com>
2022-06-26 09:05:06 -0700

    Global styles.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,11 @@
 import { AppProps } from 'next/app';
 import { ThemeProvider } from 'styled-components';
-import { GlobalStyle } from '../components/GlobalStyle';
 import { theme } from '../theme';
+import '../styles/global.css';
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
         <ThemeProvider theme={theme}>
-            <GlobalStyle />
             <Component {...pageProps} />
         </ThemeProvider>
     );
