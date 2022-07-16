8b2a74de5dd256bcc1b08fbea72d06e02898b6e6
zachsnoek <dev@zachsnoek.com>
2022-07-16 09:15:58 -0700

    fix: Test.

--- pages/404.tsx
+++ pages/404.tsx
@@ -3,7 +3,7 @@ import { MainLayout } from '../components/Layout';
 export default function Custom404() {
     return (
         <MainLayout title="404" header="Oops...">
-            <p>😬 Sorry, it looks like that page doesn&apos;t exist. 🙃</p>
+            <p>😬 Sorry, it looks like that page doesn&apos;t exist. 🤷‍♀️</p>
         </MainLayout>
     );
 }
