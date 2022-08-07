d134341a5660f9e130819fd8fafab0ab00e1e403
zachsnoek <dev@zachsnoek.com>
2022-08-07 15:41:56 -0700

    fix: Fix date formatting.

--- lib/formatDate.ts
+++ lib/formatDate.ts
@@ -1,8 +1,14 @@
 export function formatDate(date: string) {
-    if (typeof date !== 'string' || !date) {
+    if (!date || typeof date !== 'string') {
         throw new Error('Invalid date string.');
     }
-    return new Date(date).toLocaleDateString('en-US', {
+
+    const localizedDate = new Date(date);
+    const utcDate = new Date(
+        localizedDate.getTime() + localizedDate.getTimezoneOffset() * 60000
+    );
+
+    return utcDate.toLocaleDateString('en-US', {
         month: 'long',
         day: 'numeric',
         year: 'numeric',
