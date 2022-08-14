a57e892c71ffd5e8ecc4e42f982be96419a82498
zachsnoek <dev@zachsnoek.com>
2022-08-13 19:14:38 -0700

    chore: Update GoatCounter URL.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,12 +4,14 @@ import { theme } from '../theme';
 import { GlobalStyle } from '../components/GlobalStyle';
 import { GCScript } from 'next-goatcounter';
 
+const GOAT_COUNTER_URL = 'https://zachsnoek.goatcounter.com/count';
+
 export default function App({ Component, pageProps }: AppProps) {
     return (
         <ThemeProvider theme={theme}>
             <GlobalStyle />
             <Component {...pageProps} />
-            <GCScript siteUrl="https://beta-zachsnoek.goatcounter.com/count" />
+            <GCScript siteUrl={GOAT_COUNTER_URL} />
         </ThemeProvider>
     );
 }
