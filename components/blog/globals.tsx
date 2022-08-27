8860c93bb23bd9df4ba8c9e416210134295ca7db
zachsnoek <dev@zachsnoek.com>
2022-08-27 10:03:06 -0700

    feat: Add global connect section component.

--- components/blog/globals.tsx
+++ components/blog/globals.tsx
@@ -1,6 +1,8 @@
 import { CodePen as MDXEmbedCodePen } from 'mdx-embed';
 import styled from 'styled-components';
-
+import { BuyMeACoffee } from '../BuyMeACoffee';
+import { CopyableHeader } from '../CopyableHeader';
+import { a as A } from './default-replacements';
 export const CodePen = ({ id, tabs, ...props }) => (
     <CodePenWrapper>
         <MDXEmbedCodePen codePenId={id} tabs={['result', ...tabs]} {...props} />
@@ -15,3 +17,39 @@ const CodePenWrapper = styled.div`
         margin-right: 0;
     }
 `;
+
+export function ConnectSection() {
+    return (
+        <>
+            <CopyableHeader as="h2">Let&apos;s connect</CopyableHeader>
+            <p>
+                Come connect with me on{' '}
+                <A
+                    href="https://linkedin.com/in/zachsnoek"
+                    target="_blank"
+                    rel="noreferrer"
+                >
+                    LinkedIn
+                </A>
+                ,{' '}
+                <A
+                    href="https://twitter.com/zach_snoek"
+                    target="_blank"
+                    rel="noreferrer"
+                >
+                    Twitter
+                </A>
+                , and{' '}
+                <A
+                    href="https://github.com/zachsnoek"
+                    target="_blank"
+                    rel="noreferrer"
+                >
+                    GitHub
+                </A>
+                !
+            </p>
+            <BuyMeACoffee />
+        </>
+    );
+}
