36cef61844e528927b1a2b8b9d2f25ec8f53953e
zachsnoek <dev@zachsnoek.com>
2025-06-04 12:40:29 -0400

    refactor: Remove styled-components from MDX replacement components.

--- components/blog/default-replacements.tsx
+++ components/blog/default-replacements.tsx
@@ -1,4 +1,3 @@
-import styled from 'styled-components';
 import { CopyableHeader } from '../CopyableHeader/CopyableHeader';
 import { Link } from '../Link/Link';
 
@@ -16,40 +15,8 @@ export function h3(props) {
     return <CopyableHeader as="h3" {...props} />;
 }
 
-const OurOl = styled.ol`
-    padding-left: 0px;
-    margin-left: 22px; /* offsets max width container padding; subtracts 2px for optical alignment */
-`;
-export function ol(props) {
-    return <OurOl {...props} />;
-}
-
-const OurUl = styled.ul`
-    padding-left: 0px;
-    margin-left: 22px; /* offsets max width container padding; subtracts 2px for optical alignment */
-    list-style-position: outside;
-`;
-export function ul(props) {
-    return <OurUl {...props} />;
-}
-
 export function a(props) {
     return <Link {...props} />;
-}
-
-const OurBlockquote = styled.blockquote`
-    color: var(--prose-blockquote-color);
-    margin-left: 0px;
-    padding-left: 24px;
-    border-left: var(--border-width-1) solid var(--blockquote-border-left-color);
-    font-style: italic;
-`;
-export function blockquote(props) {
-    return <OurBlockquote {...props} />;
-}
-
-export function code({ className, children }) {
-    return <code className={className}>{children}</code>;
 }
 
 export function img({ src, ...props }) {
