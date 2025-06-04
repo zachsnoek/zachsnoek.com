c9cae78f587f82931e61834da767176abe934d9b
zachsnoek <dev@zachsnoek.com>
2025-06-04 14:47:15 -0400

    feat: Update landing copy.

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { GetStaticProps } from 'next';
 import { Accent } from '../components/Accent/Accent';
 import { MainLayout } from '../components/Layout';
-import { Link } from '../components/Link/Link';
 import { getAllPosts, Post } from '../utils/posts';
 import { Spacer } from '../components/Spacer';
 import { PostPreviewList } from '../components/PostPreviewList';
@@ -22,14 +21,16 @@ export default function Home({ recentPosts }: Props) {
             }
         >
             <section>
-                <p>I&apos;m a full-stack software engineer from Detroit, MI.</p>
-                <Spacer size={2} />
                 <p>
-                    I currently work at{' '}
-                    <Link href="https://aloftappraisal.com" target="_blank">
-                        Aloft, Inc.
-                    </Link>{' '}
-                    where I work with TypeScript, React.js, and Node.js.
+                    I&apos;m a product-focused senior software engineer from
+                    Detroit, MI.
+                </p>
+                <Spacer size={3} />
+                <p>
+                    I lead full-stack projects from idea to launch using React,
+                    TypeScript, Node, and SQL. I embrace ambiguity, have strong
+                    product instincts, and build with care and attention to
+                    detail.
                 </p>
             </section>
             <Spacer size={8} />
