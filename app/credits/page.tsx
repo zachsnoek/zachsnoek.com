537c46427b0bc3c41a4845f5741ec978a9411995
zachsnoek <dev@zachsnoek.com>
2025-06-04 16:24:01 -0400

    feat: Migrate credits page to App Router.

---
+++ app/credits/page.tsx
@@ -0,0 +1,21 @@
+import type { Metadata } from 'next';
+import { Link } from '../../components/Link/Link';
+import { MainContentLayout } from '../../components/MainContentLayout';
+
+export const metadata: Metadata = {
+    robots: 'noindex',
+};
+
+export default function CreditsPage() {
+    return (
+        <MainContentLayout header="Credits">
+            <p>
+                <Link target="_blank" href="https://velvetyne.fr/fonts/karrik/">
+                    Karrick
+                </Link>{' '}
+                by Jean-Baptiste Morizot and Lucas Le Bihan (Velvetyne Type
+                Foundry).
+            </p>
+        </MainContentLayout>
+    );
+}
