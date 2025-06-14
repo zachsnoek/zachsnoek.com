decfb38eeea38d8d41826332c56b7ffb62e43973
zachsnoek <dev@zachsnoek.com>
2025-06-14 17:40:16 -0400

    refactor: Clean up getAllPosts.

--- utils/getAllPosts.ts
+++ utils/getAllPosts.ts
@@ -3,44 +3,35 @@ import path from 'path';
 import { Post } from '../types';
 import { getPostMetadata } from './getPostMetadata';
 
-type GetAllPostsOptions = {
+type Options = {
     filter?: {
         tag?: string;
     };
     limit?: number;
 };
 
-export async function getAllPosts(
-    queryOptions: GetAllPostsOptions = {}
-): Promise<Post[]> {
-    const options = { limit: 100, ...queryOptions };
+export async function getAllPosts(options: Options = {}): Promise<Post[]> {
     const postDirectories = fs.readdirSync(
         path.join(process.cwd(), 'content', 'blog')
     );
 
     const { filter, limit } = options;
 
-    const posts = await Promise.all(
+    const posts: Post[] = await Promise.all(
         postDirectories.map(async (directory) => {
             const metadata = await getPostMetadata(directory);
-            if (filter?.tag && !metadata.tags.includes(filter?.tag)) {
-                return;
-            }
             const post: Post = { id: directory, ...metadata };
             return post;
         })
     );
 
-    const sortedPosts = posts
-        .filter((post): post is Post => post !== undefined)
-        .sort(({ date: a }, { date: b }) => {
-            if (a < b) {
-                return 1;
-            } else if (a > b) {
-                return -1;
-            }
-            return 0;
-        });
+    const tag = filter?.tag?.length ? filter.tag : null;
+    const filteredPosts =
+        tag !== null ? posts.filter((post) => post.tags.includes(tag)) : posts;
+
+    const sortedPosts = filteredPosts.sort(({ date: a }, { date: b }) =>
+        b.localeCompare(a)
+    );
 
     return sortedPosts.slice(0, limit);
 }
