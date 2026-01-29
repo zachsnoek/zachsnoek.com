e4c509c79f2c7c9dcafa9659bd45cb2661fec67b
zachsnoek <me@zachsnoek.com>
2026-01-29 07:27:15 -0500

    createPost: Add tips-and-lessons-learned-from-my-sabbatical.

---
+++ content/blog/tips-and-lessons-learned-from-my-sabbatical/metadata.ts
@@ -0,0 +1,9 @@
+import { PostMetadata } from '../../../types';
+
+export const metadata: PostMetadata = {
+    title: 'Financial Tips and Lessons Learned from My Post-Layoff Sabbatical',
+    date: '2026-01-29',
+    description: 'Thoughts on building and using savings after a layoff.',
+    categories: [],
+    tags: ['personal finance'],
+};
