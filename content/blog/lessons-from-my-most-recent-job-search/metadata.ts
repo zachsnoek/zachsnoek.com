be99773e7473e94c5b49ff5424772fd9108dd6e6
zachsnoek <me@zachsnoek.com>
2026-02-25 21:50:12 -0500

    create-post: add job search lessons post

---
+++ content/blog/lessons-from-my-most-recent-job-search/metadata.ts
@@ -0,0 +1,10 @@
+import { PostMetadata } from '../../../types';
+
+export const metadata: PostMetadata = {
+    title: 'Lessons from My Most Recent Job Search',
+    date: '2026-02-25',
+    description:
+        'A few things I learned during my most recent job search in 2025.',
+    categories: [],
+    tags: [],
+};
